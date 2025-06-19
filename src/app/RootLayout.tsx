5c80d7b321e75e4937a813c8c979e89d36d2840a
Hassan Rehan <hrehan752@gmail.com>
2025-06-19 18:47:12 +0500

    Update RootLayout.tsx

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -1,1 +1,14 @@
+// app/RootLayout.tsx
+"use client";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return <>{children}</>;
+}
